42e4b031a979ff80b6687bd37f02b0861cea6eb8
Gonior <43252085+Gonior@users.noreply.github.com>
2021-10-06 19:11:49 +0700

    add taiwind

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+</script>
+
+<Tailwindcss />
+<main class="bg-pink-500 h-screen">
+	
+</main>
+
+<style global lang="postcss">
+	.primary-btn {
+		@apply border-0 text-white rounded bg-blue-600 p-2;
+	} 
+	
+</style>
